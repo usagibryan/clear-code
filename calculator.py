de8a7e0aab715349a3e33f7fef0022d15d38d909
usagibryan <34449029+usagibryan@users.noreply.github.com>
2023-02-14 21:50:57 -0500

    no changes

--- calculator.py
+++ calculator.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
-#lala
 # Attach widgets to window here
 class MainWindow(qtw.QWidget):
 	def __init__(self):
