41ea2a475320b65dd347d867b3d9bf446d659097
usagibryan <34449029+usagibryan@users.noreply.github.com>
2024-02-21 14:41:50 -0500

    Fix file paths

--- zelda/tile.py
+++ zelda/tile.py
@@ -5,5 +5,5 @@ class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
 		#file path has to be './zelda/graphics/test/rock.png' in VSCode, why?
-		self.image = pygame.image.load('./zelda/graphics/test/rock.png').convert_alpha()
+		self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
