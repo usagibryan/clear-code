57a13ea4e69a082a349ec0dd84a71514e3a97a2d
usagibryan <34449029+usagibryan@users.noreply.github.com>
2024-02-23 11:03:32 -0500

    Add comment about directory issue

--- zelda/tile.py
+++ zelda/tile.py
@@ -4,6 +4,7 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
-		# self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
-		self.image = pygame.image.load('./zelda/graphics/test/rock.png').convert_alpha()
+		# run configuration current working directory
+		self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
+		# self.image = pygame.image.load('./zelda/graphics/test/rock.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
