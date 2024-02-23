e1cb1435c142a155f74240bea624a14e22b02a79
usagibryan <34449029+usagibryan@users.noreply.github.com>
2024-02-23 11:08:48 -0500

    Add JSON file

--- zelda/player.py
+++ zelda/player.py
@@ -5,7 +5,7 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
 		# run configuration current working directory
-		self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
+		self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
 		# self.image = pygame.image.load('./zelda/graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 
--- zelda/tile.py
+++ zelda/tile.py
@@ -5,6 +5,6 @@ class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
 		# run configuration current working directory
-		self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
+		self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
 		# self.image = pygame.image.load('./zelda/graphics/test/rock.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
