fa5356feef3cfaa1decce6b968189672a6a5809e
usagibryan <3444929+usagibryan@users.noreply.github.com>
2024-02-24 15:16:55 -0500

    Add collision

--- zelda/player.py
+++ zelda/player.py
@@ -2,15 +2,17 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
+	def __init__(self,pos,groups,obstacle_sprites):
 		super().__init__(groups)
 		# run configuration current working directory
-		self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
-		# self.image = pygame.image.load('./zelda/graphics/test/player.png').convert_alpha()
+		# self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
+		self.image = pygame.image.load('./zelda/graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
+
+		self.obstacle_sprites = obstacle_sprites
 
 	def input(self):
 		keys = pygame.key.get_pressed()
@@ -37,7 +39,30 @@ class Player(pygame.sprite.Sprite):
 		if self.direction.magnitude() != 0:
 			self.direction = self.direction.normalize()
 
-		self.rect.center += self.direction * speed
+		self.rect.x += self.direction.x * speed
+		self.collision('horizontal')
+		self.rect.y += self.direction.y * speed
+		self.collision('vertical')
+		# self.rect.center += self.direction * speed
+
+	def collision(self,direction):
+		if direction == 'horizontal':
+			for sprite in self.obstacle_sprites:
+				if sprite.rect.colliderect(self.rect):
+					if self.direction.x > 0: # moving right
+						self.rect.right = sprite.rect.left
+					if self.direction.x < 0: # moving left
+						self.rect.left = sprite.rect.right
+		if direction == 'vertical':
+			for sprite in self.obstacle_sprites:
+				if sprite.rect.colliderect(self.rect):
+					if self.direction.y > 0: # moving bottom
+						self.rect.bottom = sprite.rect.top
+					if self.direction.y < 0: # moving up
+						self.rect.top = sprite.rect.bottom
+		
+		if direction == 'vertical':
+			pass
 
 	def update(self):
 		self.input()
