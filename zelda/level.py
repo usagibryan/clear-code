56d6e4e2293376137069c02118f202b6ae656ac9
usagibryan <3444929+usagibryan@users.noreply.github.com>
2024-11-28 13:33:55 -0500

    Re-organize folders, delete Zelda, add Snake

--- zelda/level.py
+++ zelda/level.py
@@ -1,4 +1,6 @@
-import pygame 
+from typing import Iterable
+import pygame
+from pygame.sprite import AbstractGroup 
 from settings import *
 from tile import Tile
 from player import Player
@@ -11,7 +13,7 @@ class Level:
 		self.display_surface = pygame.display.get_surface()
 
 		# sprite group setup
-		self.visible_sprites = pygame.sprite.Group()
+		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
 		# sprite setup
@@ -29,6 +31,17 @@ class Level:
 
 	def run(self):
 		# update and draw the game
-		self.visible_sprites.draw(self.display_surface)
+		self.visible_sprites.custom_draw()
 		self.visible_sprites.update()
-		debug(self.player.direction)
+		# debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		
+		# general setup
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+
+	def custom_draw(self):
+		for sprite in self.sprites():
+			self.display_surface.blit(sprite.image,sprite.rect)
